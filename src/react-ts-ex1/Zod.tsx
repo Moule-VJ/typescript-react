4a50900cd72b89baf01fff8ad3bbbb3a7ce83e2d
moule-vj <moulevjg@gmail.com>
2024-08-21 01:06:05 +0530

    zod with enum

--- src/react-ts-ex1/Zod.tsx
+++ src/react-ts-ex1/Zod.tsx
@@ -1,10 +1,20 @@
 import { z } from "zod";
 
+enum Hobbies {
+  Programming = "Programming",
+  WeightLifting = "WeightLifting",
+  Guitar = "Guitar",
+}
+
+const arrHobbies = ["Programming", "Guitar", "Weight Lifting"] as const;
+
 export const UserSchema = z.object({
   username: z.string().min(3),
   rollNo: z.number().gt(0),
   birthDate: z.date().optional(),
   isProgrammer: z.boolean().default(true),
+  hobby: z.nativeEnum(Hobbies),
+  arrHobby: z.enum(arrHobbies),
 });
 
 type User = z.infer<typeof UserSchema>;
@@ -14,12 +24,11 @@ const user: User = {
   rollNo: 12,
   birthDate: new Date(),
   isProgrammer: true,
+  hobby: Hobbies.Programming,
+  arrHobby: "Weight Lifting",
 };
 
 console.log(UserSchema.parse(user));
 
 // Safe Parse
-
 console.log(UserSchema.safeParse(user).success);
-
-// Validation
